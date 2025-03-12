8db03fdc92c8ce7fcad5efc6f226c858888e2de4
Leonardo_mandel <Leomm2304@gmail.com>
2025-03-12 09:14:03 -0300

    interface case metodo

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,120 @@
+import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner escolha = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~ Bem Vindo ~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("Escolha uma das Opções: \n" +
-                "(1) - Listar \n" +
-                "(2) - Alterar \n" +
-                "(3) - Excluir \n" +
-                "(4) - Exibir Lista");
-
-        int opcao = escolha.nextInt();  // Lê a entrada do usuário
-
+        Menu menu = new Menu();
+        menu.imprimirMensagem(Scanner sc);
         switch (opcao) {
             case 1:
-                System.out.println("O que Você deseja listar? \n" +
-                        "");
+                System.out.println("O que Você deseja Incluir? \n" +
+                        "(1) Eletronico - (2) Roupa - (3) Alimento - (0) Cancelar");
+
+                int opcao1 = sc.nextInt();
+
+                switch (opcao1){
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                }
                 break;
+
             case 2:
-                System.out.println("Você escolheu Alterar.");
+                System.out.println("O que Você deseja Alterar? \n" +
+                    "(1) Eletronico - (2) Roupa - (3) Alimento - (0) Cancelar");
+
+                int opcao2 = sc.nextInt();
+
+                switch (opcao2){
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                }
                 break;
             case 3:
-                System.out.println("Você escolheu Excluir.");
+                System.out.println("O que Você deseja Excluir? \n" +
+                        "(1) Eletronico - (2) Roupa - (3) Alimento - (0) Cancelar");
+
+                int opcao3 = sc.nextInt();
+
+                switch (opcao3){
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                }
                 break;
             case 4:
-                System.out.println("Você escolheu Exibir Lista.");
+                System.out.println("O que Você deseja listar? \n" +
+                        "(1) Eletronico - (2) Roupa - (3) Alimento - (0) Cancelar");
+
+                int opcao0 = sc.nextInt();
+
+                switch (opcao0){
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                }
+                break;
+            case 5:
+                System.out.println("Obrigado, Volte sempre!");
                 break;
             default:
                 System.out.println("Opção inválida.");
                 break;
         }
 
-    Eletronico e1 = new Eletronico("TV","ABC",2999.99,"Samsung",12);
 
-    e1.exibirInformacoes();
+        }
+
+        public void imprimirMensagem(Scanner sc) {
+
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~ Bem Vindo ~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("Escolha uma das Opções: \n" +
+                    "(1) - Incluir \n" +
+                    "(2) - Alterar \n" +
+                    "(3) - Excluir \n" +
+                    "(4) - Listar \n" +
+                    "(5) - Sair");
+
+            int opcao = sc.nextInt();  // Lê a entrada do usuário
 
     }
 }
